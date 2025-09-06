582f0b31e69a760a80c03f669613d41a88430734
Emiliosusanu <liam.trump@aol.com>
2025-09-06 12:17:37 +0300

    fix: resolve merge conflict in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,44 @@
+import path from 'node:path';
+import react from '@vitejs/plugin-react';
+import { defineConfig, createLogger } from 'vite';
+
+const isDev = process.env.NODE_ENV !== 'production';
+let inlineEditPlugin, editModeDevPlugin;
+
+if (isDev) {
+  inlineEditPlugin = (await import('./plugins/visual-editor/vite-plugin-react-inline-editor.js')).default;
+  editModeDevPlugin = (await import('./plugins/visual-editor/vite-plugin-edit-mode.js')).default;
+}
+
+// plugin you defined earlier in your file
+const addTransformIndexHtml = {
+  name: 'add-transform-index-html',
+  transformIndexHtml(html) {
+    return {
+      html,
+      tags: [
+        {
+          tag: 'script',
+          attrs: { type: 'module' },
+          children: 'console.log("runtime error handler")',
+          injectTo: 'head',
+        }
+      ],
+    };
+  },
+};
+
+// logger override
+console.warn = () => {};
+const logger = createLogger();
+const loggerError = logger.error;
+logger.error = (msg, options) => {
+  if (options?.error?.toString().includes('CssSyntaxError: [postcss]')) {
+    return;
+  }
+  loggerError(msg, options);
+};
+
 export default defineConfig({
   customLogger: logger,
   plugins: [
